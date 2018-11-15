6073247d3e6883dd92aa22d6b1e785d9f076db81
rinjumanandhar <you@example.com>
2018-11-15 09:29:45 +0545

    server.js modified comment removed

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require("express"),
     route = require("./route/route"),
     parser= require("body-parser");
     app = express();
-//var request = url.parse(req.url, true);
-
 
 app.use(parser.json());
 app.use('/api/', route);
