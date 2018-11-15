709e11016e6a149936142f2268e949847e6be0d3
rinjumanandhar <you@example.com>
2018-11-15 07:02:24 +0545

    some line breaks added

--- route/route.js
+++ route/route.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 var postcontroller= require("./../controller/file");
 
-
 router.post('/post', postcontroller.postfunction);
 
 router.get('/get', postcontroller.getfunction);
