8341fb5ff8f8e9ed8e514a0c4c400495242c2e43
rinjumanandhar <you@example.com>
2018-11-15 07:00:10 +0545

    json response feature added on post request

--- controller/file.js
+++ controller/file.js
@@ -1,7 +1,16 @@
+var express = require("express"),
+    app = express();
+
 exports.postfunction = function (req, res) {
-   res.json({ username: 'Flavio'});
+    const response = {
+        username: req.body,
+        age: req.body
+    };
+    //console.log(req.body);
+    res.send(JSON.stringify(response));
 }; 
 
 exports.getfunction = function (req, res) {
-    res.send("Hello!");
+    res.send("Hello and Welcome!!!");
+
 };
