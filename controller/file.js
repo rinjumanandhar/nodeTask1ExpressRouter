7b8366e6163b3f1018f55c82fd162e3fd0ada39a
rinjumanandhar <you@example.com>
2018-11-16 12:19:25 +0545

    all response is send using res.json

--- controller/file.js
+++ controller/file.js
@@ -14,17 +14,19 @@ exports.create_data = function (req, res) {
         if (err) {
             return next(err);
         }
-        res.send('Product Created successfully')
+        res.json({"message":"Product Created successfully"});
     })
 }; 
 
 
 //get all the database stored values
-exports.retrieve_all = function (req, res) {
+exports.retrieve_all = function (req,res) {
 
     Response.find(function (err, response) {
-        if (err) return next(err);
-        res.send(response);         
+        if (err) {
+            return console.error(err);
+        }
+        res.json(response);         
     })
 };
 
@@ -33,17 +35,21 @@ exports.retrieve_all = function (req, res) {
 exports.retrieve_one = function (req, res) {
 
     Response.findById(req.params.id, function (err, response) {
-        if (err) return next(err);
-        res.send(response);         
+        if (err) {
+            return console.error(err);
+        }
+        res.json(response);         
     })
 };
 
 
 //update the database stored value by ID
 exports.update_data = function (req, res) {
-    Response.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, product) {
-        if (err) return next(err);
-        res.send('Product udpated.');
+    Response.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        res.json({"message":"Product udpated."});
     
     });
 };
@@ -52,7 +58,9 @@ exports.update_data = function (req, res) {
 //delete database stored values by ID
 exports.delete_data = function (req, res) {
     Response.findByIdAndRemove(req.params.id, function (err) {
-        if (err) return next(err);
-        res.send('Deleted successfully!');
+        if (err) {
+            return console.error(err);
+        }
+        res.json({"message":"Deleted successfully!"});
     })
 };
