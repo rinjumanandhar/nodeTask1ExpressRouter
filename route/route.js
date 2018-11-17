a66214df10ff7d0e4a4e92685dab198536a0419c
rinjumanandhar <you@example.com>
2018-11-17 22:21:18 +0545

    comments removed

--- route/route.js
+++ route/route.js
@@ -2,8 +2,11 @@ var express = require("express");
 var router = express.Router();
 var responses= require("./../controller/file");
 
-
-router.get('/get', postcontroller.getfunction);
+router.post('/create', responses.create_data);
+router.get('/get', responses.retrieve_all);
+router.get('/get/:id', responses.retrieve_one);
+router.put('/:id/update', responses.update_data);
+router.delete('/:id/delete', responses.delete_data);
 
 
 module.exports = router;
